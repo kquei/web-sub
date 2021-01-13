d3742fb526cfb2a760f580ce5d1661329df08e87
kquei <kquei@163.com>
2021-01-14 00:27:19 +0800

    add sql

--- server.py
+++ server.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from setting import *
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = MYSQL_DATABASE_URI
+db = SQLAlchemy(app)
+
+
+class Ss(db.Model):
+    # 定义表名
+    __tablename__ = 'sub_hash'
+    # 定义字段
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    sub_hash = db.Column(db.String(255))
 
 
 @app.route('/sub')
